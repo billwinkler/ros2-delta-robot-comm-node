b366cc4c139d2e3559ef6dee01e61597cc625726
Bill Winkler <bill.winkler@gmail.com>
2025-02-20 13:21:22 +0000

    upgraded with message interface

--- src/delta_robot_comm/delta_robot_comm/delta_robot_comm_node.py
+++ src/delta_robot_comm/delta_robot_comm/delta_robot_comm_node.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int32MultiArray
+from delta_robot_msgs.msg import DeltaRobotCmd, DeltaMotorCmd
 
 class DeltaRobotCommNode(Node):
     def __init__(self):
         super().__init__('delta_robot_comm_node')
-        # Subscribe to the /delta_robot/cmd topic
+        # Subscribe to the /delta_robot/cmd topic using our custom message
         self.subscription = self.create_subscription(
-            Int32MultiArray,
+            DeltaRobotCmd,
             '/delta_robot/cmd',
             self.cmd_callback,
             10
@@ -16,8 +16,9 @@ class DeltaRobotCommNode(Node):
         self.get_logger().info('DeltaRobotCommNode has started.')
 
     def cmd_callback(self, msg):
-        # Log the received message
-        self.get_logger().info(f"Received command: {msg.data}")
+        # Log the received command
+        for i, motor_cmd in enumerate(msg.commands):
+            self.get_logger().info(f"Command {i}: motor_id={motor_cmd.motor_id}, pulses={motor_cmd.pulses}, direction={motor_cmd.direction}")
 
 def main(args=None):
     rclpy.init(args=args)
--- src/delta_robot_comm/setup.py
+++ src/delta_robot_comm/setup.py
@@ -4,11 +4,14 @@ from setuptools import setup, find_packages
 setup(
     name='delta_robot_comm',
     version='0.0.0',
-    packages=find_packages(),
+    packages=find_packages(),  # This finds your Python package 'delta_robot_comm'
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/delta_robot_comm']),
         ('share/delta_robot_comm', ['package.xml']),
     ],
+    package_data={
+        'delta_robot_comm': ['msg/*.py'],  # Include any .py files in the msg folder
+    },
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Bill Winkler',
